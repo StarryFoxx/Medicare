0f1b74ea1d68a4dd81ed553924b7724063f559bd
Joshua Todd <joshtodd2319@yahoo.com>
2021-06-30 21:38:31 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Medicare
-Medicare Landing Page mockup with Vue.js &amp; tailwindcss
+# medicare-app
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/Lead.vue
@@ -0,0 +1,59 @@
+<template>
+  <!-- <div class="lead">
+    <div class="flex flex-col md:flex-row relative">
+      <img
+        class=" max-h-full w-full"
+        src="../assets/images/smile.png"
+        alt="smiling"
+      /> -->
+      <!-- zipcode input -->
+      <div class="lead">
+        <div
+          class="zipcode w-full flex flex-col items-center text-center lg:rounded-xl h-full"
+        >
+          <h3
+            class=" text-white font-extrabold text-3xl bg-light-blue-alt p-5 w-full lg:rounded-t-xl"
+          >
+            Find Medicare Coverage
+          </h3>
+          <div class="flex flex-col w-full bg-dark-blue-alt py-10 lg:py-5 px-10 lg:rounded-b-xl flex-grow">
+            <p
+              class="leading-relaxed mb-3 text-gray-50 font-medium text-2xl px-5"
+            >
+              Compare
+              <span class="text-gray-200 font-extrabold">Medicare Plans</span><br>in one minute!
+            </p>
+            <!-- input area -->
+            <div class="flex flex-col bg-white p-5 rounded-xl">
+              <h5 class="text-blue font-bold text-lg">Enter Your Zip Code</h5>
+              <input
+                type="text"
+                class="text-center w-5/6 py-4 border-black border-2 mt-2 mb-3 mx-auto rounded-md"
+                placeholder=""
+              />
+              <a
+                href="#"
+                class="btn w-2/3 mx-auto text-center text-white font-bold text-center text-center text-xl rounded-md"
+                >GET STARTED</a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- zipcode input end -->
+    <!-- </div>
+  </div> -->
+</template>
+
+
+
+<script>
+export default {
+  name: 'Lead',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
